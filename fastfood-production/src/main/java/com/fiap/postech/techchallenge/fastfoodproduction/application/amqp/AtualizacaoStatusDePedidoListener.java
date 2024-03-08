867a1f9526a67283e17fe30b439e23a2475ec939
LineNicacio <line.nicacio@hotmail.com>
2024-03-07 21:03:36 -0300

    feat: testes

--- fastfood-production/src/main/java/com/fiap/postech/techchallenge/fastfoodproduction/application/amqp/AtualizacaoStatusDePedidoListener.java
+++ fastfood-production/src/main/java/com/fiap/postech/techchallenge/fastfoodproduction/application/amqp/AtualizacaoStatusDePedidoListener.java
@@ -13,6 +13,7 @@ import static com.fiap.postech.techchallenge.fastfoodproduction.infra.config.amq
 @Component
 public class AtualizacaoStatusDePedidoListener {
 
+    //FIXME: Provavelmente nao precisemos desse listener, temos o endpoint que ja chama o usecase de atualizaçao de pedido, não faria muito sentido o endpoint chamar uma use case, que posta via fila para a fila chamar a use case de atualizacao.
     private final AtualizacaoDePedido atualizacaoDePedido;
 
     public AtualizacaoStatusDePedidoListener(AtualizacaoDePedido atualizacaoDePedido) {
