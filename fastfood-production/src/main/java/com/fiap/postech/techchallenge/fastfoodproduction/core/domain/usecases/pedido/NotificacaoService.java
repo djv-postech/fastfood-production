91005dfdf6d03b05fe147d9c3bd2c51d0217fa4e
LineNicacio <line.nicacio@hotmail.com>
2024-03-09 23:21:59 -0300

    fix: numeroPedido

--- fastfood-production/src/main/java/com/fiap/postech/techchallenge/fastfoodproduction/core/domain/usecases/pedido/NotificacaoService.java
+++ fastfood-production/src/main/java/com/fiap/postech/techchallenge/fastfoodproduction/core/domain/usecases/pedido/NotificacaoService.java
@@ -1,10 +1,12 @@
 package com.fiap.postech.techchallenge.fastfoodproduction.core.domain.usecases.pedido;
 
 import com.fiap.postech.techchallenge.fastfoodproduction.application.records.DadosNotificacao;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 import static com.fiap.postech.techchallenge.fastfoodproduction.infra.config.amqp.ProducaoAMQPConfiguration.NOTIFICACAO_EX;
 
+@Slf4j
 public class NotificacaoService {
 
     private RabbitTemplate rabbitTemplate;
@@ -15,5 +17,8 @@ public class NotificacaoService {
 
     public void enviarParaFilaDeNotificacao(DadosNotificacao dadosNotificacao) {
         rabbitTemplate.convertAndSend(NOTIFICACAO_EX, "", dadosNotificacao);
+
+        log.info("Notificação enviada! Payload: {}", dadosNotificacao);
+
     }
 }
